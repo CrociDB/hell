0699c8e97e69af41d40738a8b658296f4364aca4
Bruno Croci <crocidb@gmail.com>
2025-05-27 15:31:20 +0000

    separating commands, adding echo

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,47 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+fn commands_exit(line: Vec<&str>) {
+    if line.len() < 2 {
+        std::process::exit(1);
+    }
+
+    let value: i32 = match line[1].trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            eprintln!(
+                "Error: Unable to parse exit parameter '{}' as an integer",
+                line[1]
+            );
+            return;
+        }
+    };
+
+    std::process::exit(value);
+}
+
+fn commands_echo(line: Vec<&str>) {
+    if line.len() < 2 {
+        println!("");
+    }
+
+    let joined: String = line[1..].join(" ");
+    println!("{}", joined.trim());
+}
+
+fn interpret_line(input: String) {
+    let cl: Vec<&str> = input.split(' ').collect();
+
+    let comm = cl[0].trim();
+    if comm == "exit" {
+        commands_exit(cl);
+    } else if comm == "echo" {
+        commands_echo(cl);
+    } else {
+        println!("{}: command not found", input.trim());
+    }
+}
+
 fn main() {
     loop {
         // Uncomment this block to pass the first stage
@@ -10,11 +51,6 @@ fn main() {
         // Wait for user input
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
-
-        if input.trim() == "exit 0" {
-            std::process::exit(0);
-        } else {
-            println!("{}: command not found", input.trim());
-        }
+        interpret_line(input);
     }
 }
