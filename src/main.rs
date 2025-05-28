b72e0749c70ef45df44aa89dbff0ebd12531d0db
Bruno Croci <crocidb@gmail.com>
2025-05-28 07:19:33 +0000

    separating builtin in its own module

--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,13 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-// Builtins
-
-struct BuiltinCommand {
-    command: &'static str,
-    func: fn(Vec<&str>),
-}
-
-const BUILTIN_COMMANDS: &[BuiltinCommand] = &[
-    BuiltinCommand {
-        command: "echo",
-        func: builtin_echo,
-    },
-    BuiltinCommand {
-        command: "exit",
-        func: builtin_exit,
-    },
-    BuiltinCommand {
-        command: "type",
-        func: builtin_type,
-    }
-];
-
-fn builtin_echo(line: Vec<&str>) {
-    if line.len() < 2 {
-        println!(" ");
-    }
-
-    let joined: String = line[1..].join(" ");
-    println!("{}", joined.trim());
-}
-
-fn builtin_exit(line: Vec<&str>) {
-    if line.len() < 2 {
-        std::process::exit(1);
-    }
-
-    let value: i32 = match line[1].trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            eprintln!(
-                "Error: Unable to parse exit parameter '{}' as an integer",
-                line[1]
-            );
-            return;
-        }
-    };
-
-    std::process::exit(value);
-}
-
-fn builtin_type(line: Vec<&str>) {
-    if line.len() < 2 {
-        eprintln!("Error: command `type` requirest one parameter");
-        return;
-    }
-
-    let mut builtin_found = false;
-    for builtin in BUILTIN_COMMANDS {
-        if line[1].trim() == builtin.command {
-            println!("{} is a shell builtin", builtin.command);
-            builtin_found = true;
-            break;
-        }
-    }
-
-    if !builtin_found {
-        println!("{}: not found", line[1].trim());
-    }
-}
-
-fn check_builtins(line: Vec<&str>) -> bool {
-    for builtin in BUILTIN_COMMANDS {
-        let comm = line[0].trim();
-        if comm == builtin.command {
-            (builtin.func)(line);
-            return true;
-        }
-    }
-
-    false
-}
-
-// Line check
+mod builtin;
 
 fn interpret_line(input: String) {
     let cl: Vec<&str> = input.split(' ').collect();
 
     // let comm = cl[0].trim();
-    if !check_builtins(cl) {
+    if !builtin::check_builtins(cl) {
         println!("{}: command not found", input.trim());
     }
 }
